d9d0e8375db861afea7c2f84d1a93bd090da9832
SkRhRasel <sk.rasel.cse@gmail.com>
2019-03-27 23:42:38 +0600

    Shake to Change color

---
+++ app/src/main/java/com/example/shaketochangecolor/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.shaketochangecolor;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean isColor = false;
+    private View view;
+    private long lastUpdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textView);
+        view.setBackgroundColor(Color.CYAN);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            getAccelerometer(sensorEvent);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private void getAccelerometer(SensorEvent sensorEvent) {
+        float[] values = sensorEvent.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x*x+y*y+z*z)/ (SensorManager.GRAVITY_EARTH*SensorManager.GRAVITY_EARTH);
+
+        long actualTime = System.currentTimeMillis();
+        Toast.makeText(getApplicationContext(), String.valueOf(accelationSquareRoot)+" "+
+                SensorManager.GRAVITY_EARTH, Toast.LENGTH_SHORT).show();
+
+        if (accelationSquareRoot >= 2) //it will be executed if you shuffle
+        {
+
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;//updating lastUpdate for next shuffle
+            if (isColor) {
+                view.setBackgroundColor(Color.GREEN);
+
+            } else {
+                view.setBackgroundColor(Color.RED);
+            }
+            isColor = !isColor;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
+
